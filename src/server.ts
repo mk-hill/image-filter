67cc9191e7f0519f80381576292b39e00d8ab476
mk-hill <40850298+mk-hill@users.noreply.github.com>
2020-04-21 21:21:58 -0500

    Add filtered image endpoint, update filter util

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { filterImageFromURL, deleteLocalFiles, errorMessages } from './util/util';
 
 (async () => {
-
   // Init the Express application
   const app = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
-  
+
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
@@ -22,25 +21,40 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
   //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
+  // QUERY PARAMETERS
   //    image_url: URL of a publicly accessible image
   // RETURNS
   //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get('/filteredimage', async (req, res) => {
+    const { image_url: url } = req.query as { image_url: string };
+    if (!url) {
+      return res.status(400).send('Image URL required');
+    }
+
+    try {
+      const localPath = await filterImageFromURL(url);
+      res.status(200).sendFile(localPath);
+      res.on('finish', deleteLocalFiles.bind(null, [localPath]));
+    } catch ({ message }) {
+      if (!Object.values(errorMessages).includes(message)) {
+        message = 'Internal error'; // Default to avoid sending unknown error messages to user
+      }
+      const statusCode = message === errorMessages.READ ? 400 : 500; // Assume url was the problem if Jimp couldn't read
+      res.status(statusCode).send(message);
+    }
+  });
 
   /**************************************************************************** */
 
-  //! END @TODO1
-  
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
-  } );
-  
+  app.get('/', async (req, res) => {
+    res.send('try GET /filteredimage?image_url={{}}');
+  });
 
   // Start the Server
-  app.listen( port, () => {
-      console.log( `server running http://localhost:${ port }` );
-      console.log( `press CTRL+C to stop server` );
-  } );
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
 })();
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,6 +1,11 @@
 import fs from 'fs';
 import Jimp = require('jimp');
 
+export enum errorMessages {
+  READ = 'Unable to read image URL, please ensure resource exists.',
+  FILTER = 'Unable to filter image.',
+}
+
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -8,17 +13,24 @@ import Jimp = require('jimp');
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string>{
-    return new Promise( async resolve => {
-        const photo = await Jimp.read(inputURL);
-        const outpath = '/tmp/filtered.'+Math.floor(Math.random() * 2000)+'.jpg';
-        await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname+outpath, (img)=>{
-            resolve(__dirname+outpath);
-        });
+export async function filterImageFromURL(inputURL: string): Promise<string> {
+  const photo = await Jimp.read(inputURL).catch((e) => {
+    console.log(e);
+    throw new Error(errorMessages.READ);
+  });
+
+  const relativePath = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg';
+  const absolutePath = __dirname + relativePath;
+
+  return photo
+    .resize(256, Jimp.AUTO) // resize
+    .quality(60) // set JPEG quality
+    .greyscale() // set greyscale
+    .writeAsync(absolutePath)
+    .then((_) => absolutePath)
+    .catch((e) => {
+      console.log(e);
+      throw new Error(errorMessages.FILTER);
     });
 }
 
@@ -27,8 +39,10 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
-export async function deleteLocalFiles(files:Array<string>){
-    for( let file of files) {
-        fs.unlinkSync(file);
-    }
+export function deleteLocalFiles(files: string[]) {
+  for (const file of files) {
+    deleteLocalFile(file);
+  }
 }
+
+export const deleteLocalFile = (path: string) => fs.unlinkSync(path);
